e563d3b81aa0c31a49ce37dd7f49a813f35a9cff
Daniel Shiffman <daniel@shiffman.net>
2014-09-08 14:43:55 -0400

    comments and cleanup

--- randomizer.js
+++ randomizer.js
@@ -1,55 +1,72 @@
 
+// The names
 var names;
-
+// Divs to hold names
 var divs;
 
+// Canvas for wheel
 var canvas;
+
+// Stuff for name input
 var inputA;
 var input;
 
+// Size of circle
 var w;
 
+// Spin button
 var button;
 
+// Angle and speed
 var angle;
 var dangle = 0;
 
+// Size of pie slice
 var sz = 0;
+
+// Total number of students
 var total = 0;
 
+// Entry state
 var state = 0;
 
 function setup() {
 
+  // Make the canvas full screen size
   canvas = createCanvas(windowWidth,windowHeight);
-  w = windowHeight-200;
   canvas.position(0,0);
 
+  // circle is 200 pixels less than height
+  w = windowHeight-200;
+
+  // Spin button
   button = createButton("spin the wheel");
   button.id('spin');
-  button.position(windowWidth/12 - button.elt.clientWidth/2, windowHeight/2 - button.elt.clientHeight/2);
+  button.position(20, windowHeight - 50);
   button.mousePressed(spin);
 
+  // Name input
   inputA = createA("#","Enter names");
-  inputA.position(10,10);
+  inputA.position(20,10);
   inputA.style("color","#FFFFFF");
-  inputA.mousePressed(show);
-
-  input = createElement("textArea","One name per line");
+  inputA.mousePressed(inputClick);
+  input = createElement("textArea","1 per line\n2\n3\n4\n");
   input.position(8,10);
   input.attribute("rows",20);
   input.hide();
 
+  // First pass at names
   createNames();
+
+  // Random start to arrow
   angle = random(TWO_PI);
 }
 
 function draw() {
-  if (angle < 0) {
-    angle += TWO_PI;
-  }
 
   //background(0);
+
+  // clear background
   clear();
   strokeWeight(8);
   stroke(255);
@@ -57,30 +74,38 @@ function draw() {
   translate(width/2, height/2);
   ellipse(0, 0, w, w);
 
+  // For every slice
   for (var i = 0; i < total; i++) {
     push();
+    // ALternate fill color
     if (i % 2 == 0) fill(175);
     else fill(75);
-    var testAngle = angle-sz/2;
 
+    // Where is the arrow?
+    var testAngle = angle-sz/2;
     var begin = (i);
     var end = (i+1);
     
+    // Which slice
     var which = (i+1)%total;
+
+    // Is it inside the slice?
     if ((testAngle >= begin*sz && testAngle < end*sz) || (testAngle < 0 && i == total-1)) {
-      //fill(0, 255, 0, 127);
       divs[which].style("background-color","#FFFFFF");
       divs[which].style("color","#000000");
     } else {
       divs[which].style("background-color","#000000");
       divs[(i+1) % total].style("color","#FFFFFF");
     }
-
+    
+    // Draw slice
     rotate(sz*i+sz/2);
     noStroke();
     arc(0, 0, w, w, 0, sz);
     pop();
   }
+  
+  // This is just some lines to separate the slices
   for (var i = 0; i < total; i++) {
     push();
     strokeWeight(2);
@@ -89,43 +114,59 @@ function draw() {
     line(0, 0, w/2, 0);
     pop();
   }
-  rotate(angle);
-
   
+
+  // The spinner
+  rotate(angle);
+  // Length
+  var spinnerRad = 0.35*w;
+
   strokeWeight(8);
   stroke(255);
-  line(-0.35*w, 0, 0.35*w, 0);
+  line(-spinnerRad, 0, spinnerRad, 0);
   
   fill(255);
-  triangle(0.35*w, 0, 0.35*w - 24, -12, 0.35*w - 24, +12);
-  rect(-0.35*w, -6, 12,12);
+  // Arbitrary way to draw arrow
+  triangle(spinnerRad, 0, spinnerRad - 24, -12, spinnerRad - 24, 12);
+  // Back of spinner
+  rect(-spinnerRad, -6, 12,12);
+  // Center of spinner
   ellipse(0,0,12,12);
 
+  // Spin
   angle += dangle;
+  // Slow down to stop
   dangle *= 0.99;
   if (dangle < 0.001) dangle = 0;
+  // Stay between 0 and 360
   if (angle > TWO_PI) {
     angle -= TWO_PI;
   }
 }
 
+// Make the divs that show the names
 function createNames() {
+  // INitial state
   if (!names) {
     names = [];
     for (var i = 0; i < 16; i++) {
       names[i] = String(i+1);
     }
   }
+
+
   total = names.length;
-  sz = TWO_PI / total;
-  
+  sz = TWO_PI / total;   // Size of each slice
+   
+  // Clear existing divs
   if (divs) {
     for (var i = 0; i < divs.length; i++) {
       divs[i].remove();
     }
   }
 
-
+  
+  // Make new divs
   divs = [];
   var theta = 0;
   for (var i = 0; i < names.length; i++) {
@@ -143,7 +184,8 @@ function createNames() {
   }
 }
 
-function show() {
+// Input link was clicked
+function inputClick() {
   if (state === 0) {
     input.show();
     inputA.html("Submit names")
@@ -152,6 +194,15 @@ function show() {
   } else if (state === 1) {
     var data = input.value();
     names = data.split('\n');
+
+    // If blank
+    for (var i = names.length-1; i >= 0; i--) {
+      names[i] = names[i].trim();
+      if (names[i].length === 0) {
+        names.splice(i,1);
+      }
+    }
+
     createNames();
     state = 0;
     inputA.html("Enter names");
@@ -160,7 +211,8 @@ function show() {
   }
 }
 
+// A random spin
 function spin() {
-  var force = random(0.4,0.4);
+  var force = random(0.4,0.5);
   dangle += force;
 }
