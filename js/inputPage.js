24622b09e597baa3bb6bb7dd597a130fa640714c
firedrgn <holtjustin2@gmail.com>
2017-09-29 21:04:06 -0400

    Beautified the main interface and added input checks.

--- js/inputPage.js
+++ js/inputPage.js
@@ -1,24 +1,33 @@
+let input;
+let submit;
+let result;
+
 function setup() {
     noCanvas();
-    var config = {
+
+    let config = {
         apiKey: "AIzaSyA-VyZJOZVqXZj82wvVMkfJedDEhqXcIh8",
         authDomain: "a2zitp-6519b.firebaseapp.com",
         databaseURL: "https://a2zitp-6519b.firebaseio.com",
         storageBucket: "a2zitp-6519b.appspot.com",
         messagingSenderId: "363965061200"
     };
+
     firebase.initializeApp(config);
     database = firebase.database();
 
-    var input = select('#names');
-    var submit = select('#submit');
-    var result = select('#result');
+    input = select('#names');
+    submit = select('#submit');
+    result = select('#result');
 
-    submit.mousePressed(function () {
-        var data = input.value();
+    checkInput();
+
+    submit.mousePressed(() => {
+        let data = input.value();
         names = data.split('\n');
+
         // If blank
-        for (var i = names.length - 1; i >= 0; i--) {
+        for (let i = names.length - 1; i >= 0; i--) {
             names[i] = names[i].trim();
             if (names[i].length === 0) {
                 names.splice(i, 1);
@@ -27,4 +36,18 @@ function setup() {
 
         sendToFirebase(names, result);
     });
+
+    input.input(() => {
+      checkInput(input, submit);
+    });
+}
+
+function checkInput() {
+  // Check if the input is invalid and disable the button
+  if (input.value().trim().length <= 0) {
+    submit.attribute('disabled', true);
+    result.html('');
+  } else {
+    submit.removeAttribute('disabled');
+  }
 }
